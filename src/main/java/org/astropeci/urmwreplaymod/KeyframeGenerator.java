c26fb4271efc91363dafdb7d6405ba334cc392bb
LlewVallis <llewvallis@gmail.com>
2020-05-14 00:00:27 +1000

    Add trimming for both start and end of clip

--- src/main/java/org/astropeci/urmwreplaymod/KeyframeGenerator.java
+++ src/main/java/org/astropeci/urmwreplaymod/KeyframeGenerator.java
@@ -21,6 +21,7 @@ import de.johni0702.minecraft.gui.utils.lwjgl.ReadableColor;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.PacketByteBuf;
@@ -28,7 +29,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class KeyframeGenerator {
@@ -36,6 +41,22 @@ public class KeyframeGenerator {
     private final SPTimeline timeline;
     private final ReplayHandler replayHandler;
     private final Logger logger = LogManager.getLogger();
+
+    @Value
+    private static class Span {
+        int start;
+        int end;
+    }
+
+    @Value
+    private static class Event {
+        int time;
+        Type type;
+
+        public enum Type {
+            START, END
+        }
+    }
 
     public boolean generate() {
         UUID spectatedUuid = ReplayModReplay.instance.getReplayHandler().getSpectatedUUID();
@@ -45,11 +66,38 @@ public class KeyframeGenerator {
             return false;
         }
 
-        int startTime = findStartTime();
-        int endTime = replayHandler.getReplayDuration();
+        int duration = replayHandler.getReplayDuration();
 
-        if (startTime > endTime) {
-            startTime = 0;
+        List<Integer> startTimes = findStartTimes();
+        List<Integer> endTimes = findEndTimes(duration);
+
+        List<Event> events = new ArrayList<>();
+        events.addAll(startTimes.stream().map(time -> new Event(time, Event.Type.START)).collect(Collectors.toList()));
+        events.addAll(endTimes.stream().map(time -> new Event(time, Event.Type.END)).collect(Collectors.toList()));
+        events.sort(Comparator.comparingInt(event -> event.time));
+
+        List<Span> spans = new ArrayList<>();
+        int lastStartTime = 0;
+        for (Event event : events) {
+            if (event.type == Event.Type.START) {
+                lastStartTime = event.time;
+            } else {
+                Span span = new Span(lastStartTime, event.time);
+                spans.add(span);
+                lastStartTime = event.time;
+            }
+        }
+
+        Span longestSpan = spans.stream()
+                .max(Comparator.comparingInt(span -> span.end - span.start))
+                // Shouldn't ever need, but its here if we do.
+                .orElse(new Span(0, duration));
+
+        int startTime = longestSpan.start;
+        int endTime = longestSpan.end + 10000;
+
+        if (endTime > duration) {
+            endTime = duration;
         }
 
         PlayerEntity spectated = replayHandler.getCameraEntity().getEntityWorld().getPlayerByUuid(spectatedUuid);
@@ -79,7 +127,10 @@ public class KeyframeGenerator {
         return true;
     }
 
-    private int findStartTime() {
+    private List<Integer> findStartTimes() {
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+
         try {
             ReplayFile file = replayHandler.getReplayFile();
             PacketTypeRegistry packetRegistry = PacketTypeRegistry.get(ProtocolVersion.v1_15_2, State.PLAY);
@@ -102,7 +153,7 @@ public class KeyframeGenerator {
 
                     Text message = packetBuf.readText();
                     if (message.asFormattedString().contains("§bGame has started!")) {
-                        return (int) data.getTime();
+                        result.add((int) data.getTime());
                     }
                 }
             }
@@ -110,7 +161,53 @@ public class KeyframeGenerator {
             throw new RuntimeException(e);
         }
 
-        return 0;
+        return result;
+    }
+
+    private List<Integer> findEndTimes(int duration) {
+        List<Integer> result = new ArrayList<>();
+
+        try {
+            ReplayFile file = replayHandler.getReplayFile();
+            PacketTypeRegistry packetRegistry = PacketTypeRegistry.get(ProtocolVersion.v1_15_2, State.PLAY);
+
+            ReplayInputStream input = file.getPacketData(packetRegistry);
+
+            PacketData data;
+            while ((data = input.readPacket()) != null) {
+                Packet packet = data.getPacket();
+
+                int capacity = packet.getBuf().capacity();
+                byte[] bytes = new byte[capacity];
+                packet.getBuf().getBytes(0, bytes);
+                ByteBuf byteBuf = Unpooled.wrappedBuffer(bytes);
+                PacketByteBuf packetBuf = new PacketByteBuf(byteBuf);
+
+                // Title packet has ID 0x50
+                if (packet.getId() == 0x50) {
+                    // Skip magic byte
+                    packetBuf.readByte();
+
+                    int action = packetBuf.readVarInt();
+
+                    // Standard title
+                    if (action == 0) {
+                        Text message = packetBuf.readText();
+                        if (message.asFormattedString().contains("Team Wins!")) {
+                            result.add((int) data.getTime());
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (!result.contains(duration)) {
+            result.add(duration);
+        }
+
+        return result;
     }
 
     private void infoPopup(String text) {
